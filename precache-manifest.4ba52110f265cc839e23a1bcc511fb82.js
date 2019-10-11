d51b7c50dc5e0373ec495730d948fe4a5d26c03c
Adel Stiti <adelstiti@users.noreply.github.com>
2019-10-11 11:19:02 +0100

    Updates

---
+++ precache-manifest.4ba52110f265cc839e23a1bcc511fb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6ba2ac4adc2223fc3a1886bafb23d7",
+    "url": "/WeatherNow/index.html"
+  },
+  {
+    "revision": "6bfbaa9e86976dc0fcb6",
+    "url": "/WeatherNow/static/css/main.d80221f4.chunk.css"
+  },
+  {
+    "revision": "efd346b3d4d8b6b6b903",
+    "url": "/WeatherNow/static/js/2.68d5fd6c.chunk.js"
+  },
+  {
+    "revision": "6bfbaa9e86976dc0fcb6",
+    "url": "/WeatherNow/static/js/main.4c472851.chunk.js"
+  },
+  {
+    "revision": "9838ccb1a18182e12507",
+    "url": "/WeatherNow/static/js/runtime~main.064b7f30.js"
+  }
+]);
